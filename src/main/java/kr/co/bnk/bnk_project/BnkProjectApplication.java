52e71f5a155e9d1a1f0dac5044a78aa36292396f
sundae1014 <link8430@gmail.com>
2025-11-05 11:15:37 +0900

    project ex

---
+++ src/main/java/kr/co/bnk/bnk_project/BnkProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bnk.bnk_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnkProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/bnk/bnk_project/BnkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bnk.bnk_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
